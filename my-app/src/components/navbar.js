93c14b5e18b8b4bd707649a8aecf652acfa5a37f
kalhollywood <kal.hollywood@gmail.com>
2022-06-20 16:03:11 +0100

    working on navigation - incomplete

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/home">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
