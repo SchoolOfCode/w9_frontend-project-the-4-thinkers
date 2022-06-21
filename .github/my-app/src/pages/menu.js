870372cf7d6457111f1cc0bd36d27e4f388b99d0
kalhollywood <kal.hollywood@gmail.com>
2022-06-21 16:15:47 +0100

    dropdown showing array

--- .github/my-app/src/pages/menu.js
+++ .github/my-app/src/pages/menu.js
@@ -6,79 +6,87 @@ import OptionList from "../components/Dropdown2/OptionList/OptionList.js";
 import { menuItems } from "../components/Dropdown2/Weeks.js";
 import MenuItems from "../components/Dropdown2/MenuItems.js";
 
-const Menu = () => {
-  const [value, setValue] = useState("");
-  const handleSelect = (e) => {
-    console.log(e.target.value);
-    setValue(e.target.value);
-  };
-  return (
-    <header>
-      <div className="nav-area">
-        <a href="/#" className="logo">
-          Logo
-        </a>
-        <OptionList />;
-      </div>
-    </header>
-  );
-};
+// const Menu = () => {
+//   const [selected, setSelected] = useState("");
 
-export default Menu;
+//   const handleChange = (event) => {
+//     console.log(event.target.value);
+//     console.log(event.target);
+//     return (
+//       <header>
+//         <div className="nav-area">
+//           <a href="/#" className="logo">
+//             Logo
+//           </a>
+//           <OptionList />;
+//           <select value={selected} onChange={handleChange}>
+//             {menuItems.map((option) => (
+//               <option key={option.value} value={option.value}>
+//                 {option.text}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//       </header>
+//     );
+//   };
+// };
+
+// export default Menu;
 
 // Display title of page
 // Display an image
 // Render the Dropdown component
 // 5 option buttons that rely on state change
 
-// export default function Menu() {
-// const [value, setValue] = useState("");
-// const handleSelect = (e) => {
-//   console.log(e.target.value);
-//   setValue(e.target.value);
-// };
+export default function Menu() {
+  const [value, setValue] = useState("");
+  const handleSelect = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  };
 
-//   const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState("");
 
-//   const handleChange = (event) => {
-//     console.log(event.target.value);
-//     console.log(event.target);
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    console.log(event.target);
 
-//     setSelected(event.target.value);
-//   };
-//   const options = [
-//     { value: "", text: "--Choose a week--" },
-//     { value: "week1", text: "Week 1" },
-//     { value: "week2", text: "Week 2" },
-//     { value: "week3", text: "Week 3" },
-//   ];
+    setSelected(event.target.value);
+  };
+  const options = [
+    { value: "", text: "--Choose a week--" },
+    { value: "week1", text: "Week 1" },
+    { value: "week2", text: "Week 2" },
+    { value: "week3", text: "Week 3" },
+  ];
 
-//   return (
-//     <div>
-//       <h1> School of Code </h1>
-//       <img
-//         src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
-//         alt="school of code logo"
-//         id="logo"
-//       />
-//       <h2> Recap Resources</h2>
-//       <p id="select_week">Select week:</p>
-{
-  /* <Dropdown
-  formLabel="Choose a week"
-  value={menuItems}
-  onChange={handleSelect}
-  action="http://localhost:3000/resources"
-></Dropdown>; */
+  return (
+    <div>
+      <h1> School of Code </h1>
+      <img
+        src="https://d33wubrfki0l68.cloudfront.net/e6fddcbea146f91d2f3c160f7d56a9391a4740b0/4e758/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png"
+        alt="school of code logo"
+        id="logo"
+      />
+      <h2> Recap Resources</h2>
+      <p id="select_week">Select week:</p>
+      {
+        <Dropdown
+          formLabel="Choose a week"
+          value={menuItems}
+          onChange={handleSelect}
+          action="http://localhost:3000/resources"
+        ></Dropdown>
+      }
+      <p id="choice">You selected {setValue}!</p>
+      <select value={selected} onChange={handleChange}>
+        {menuItems.map((items) => (
+          <option key={items.value} value={items.value}>
+            {items.week}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
-//       <p id="choice">You selected {setValue}!</p>
-//       <select value={selected} onChange={handleChange}>
-//         {options.map((option) => (
-//           <option key={option.value} value={option.value}>
-//             {option.text}
-//           </option>
-//         ))}
-//       </select>
-//     </div>
-//   );
-// }
