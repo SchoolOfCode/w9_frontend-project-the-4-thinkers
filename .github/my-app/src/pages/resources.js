c75ef167ef994ee45e6ca11b56136e9140d40f81
ZainabAbid1 <103643063+ZainabAbid1@users.noreply.github.com>
2022-06-22 11:19:55 +0100

    added use effect

--- .github/my-app/src/pages/resources.js
+++ .github/my-app/src/pages/resources.js
@@ -8,20 +8,24 @@ import CheckBoxComponent from "../components/checkbox/checkboxcomp";
 //import CommentForm from "../components/comments/commentform";
 import CommentList from "../components/comments/commentlist";
 //import { Dropdown } from "";
-import { useState,useEffect } from "react";
+import { useState } from "react";
 
   
 export default function Resources() {
     const [commentListx, setCommentListx]=useState([]);
     
-    useEffect(async function getComments (){
+    useEffect(()=> {
+        
+        const getComments = async () => {
         const response = await fetch('http://localhost:3000/page/1')
         console.log("This is the response:", response);
         const data = await response.json();
         console.log("And this is the data: ", data);
         setCommentListx(data)
-    },[commentListx])
-    getComments();
+        };
+        getComments();
+    }, [commentListx]);
+
     return (
         <div>
             <h1 className="resources-heading"> School of Code resources </h1>
